c550f568d0aed41e6934d627bef2ed09bcb6569e
liaoipeng <liaoipeng@icloud.com>
2025-09-12 16:39:44 +0800

    Initial Commit

---
+++ logolf/logolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  logolfApp.swift
+//  logolf
+//
+//  Created by 廖逸芃 on 2025/9/12.
+//
+
+import SwiftUI
+
+@main
+struct logolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
